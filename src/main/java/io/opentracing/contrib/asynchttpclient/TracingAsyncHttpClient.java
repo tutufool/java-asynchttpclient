d86b14961b6e029be3282dae4feb1e4f42f8f4f8
tutufool <tutufool@hotmail.com>
2018-04-09 23:39:44 +0800

    Update TracingAsyncHttpClient.java

--- src/main/java/io/opentracing/contrib/asynchttpclient/TracingAsyncHttpClient.java
+++ src/main/java/io/opentracing/contrib/asynchttpclient/TracingAsyncHttpClient.java
@@ -1,177 +1,231 @@
 package io.opentracing.contrib.asynchttpclient;
 
-import io.opentracing.Span;
-import io.opentracing.Tracer;
-import io.opentracing.propagation.Format;
-import io.opentracing.propagation.TextMap;
-import org.asynchttpclient.*;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+import org.asynchttpclient.AsyncCompletionHandlerBase;
+import org.asynchttpclient.AsyncHandler;
+import org.asynchttpclient.AsyncHttpClientConfig;
+import org.asynchttpclient.DefaultAsyncHttpClient;
+import org.asynchttpclient.DefaultAsyncHttpClientConfig;
+import org.asynchttpclient.HttpResponseBodyPart;
+import org.asynchttpclient.HttpResponseStatus;
+import org.asynchttpclient.ListenableFuture;
+import org.asynchttpclient.Request;
+import org.asynchttpclient.RequestBuilder;
+import org.asynchttpclient.Response;
+
+import io.netty.handler.codec.http.HttpHeaders;
+import io.opentracing.Scope;
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import io.opentracing.propagation.Format;
+import io.opentracing.propagation.TextMap;
+import io.opentracing.util.GlobalTracer;
+
 /**
- * An {@link org.asynchttpclient.AsyncHttpClient} that traces HTTP calls using the OpenTracing API.
+ * An {@link org.asynchttpclient.AsyncHttpClient} that traces HTTP calls using
+ * the OpenTracing API.
  */
 @SuppressWarnings("unused")
 public class TracingAsyncHttpClient extends DefaultAsyncHttpClient {
 
-    private Tracer tracer;
-    private ActiveSpanSource activeSpanSource;
-    private SpanDecorator spanDecorator;
-    private List<CompletableFuture<?>> traceFutures;
+	private Tracer tracer;
+	private SpanDecorator spanDecorator;
+	private List<CompletableFuture<?>> traceFutures;
 
-    @SuppressWarnings({"WeakerAccess", "unused"})
-    public TracingAsyncHttpClient(Tracer tracer, ActiveSpanSource activeSpanSource, SpanDecorator spanDecorator) {
-        this(tracer, activeSpanSource, spanDecorator, new DefaultAsyncHttpClientConfig.Builder().build());
-    }
+	public TracingAsyncHttpClient(SpanDecorator spanDecorator) {
+		this(GlobalTracer.get(), spanDecorator);
+	}
 
-    @SuppressWarnings("WeakerAccess")
-    public TracingAsyncHttpClient(Tracer tracer, ActiveSpanSource activeSpanSource, SpanDecorator spanDecorator, AsyncHttpClientConfig config) {
-        super(config);
-        this.tracer = tracer;
-        this.activeSpanSource = activeSpanSource;
-        this.spanDecorator = spanDecorator;
-        // Retain a reference to futures so they aren't GC'ed before completion.
-        this.traceFutures = new ArrayList<>();
-    }
+	public TracingAsyncHttpClient(Tracer tracer, SpanDecorator spanDecorator) {
+		this(tracer, spanDecorator, new DefaultAsyncHttpClientConfig.Builder().build());
+	}
 
-    @Override
-    public ListenableFuture<Response> executeRequest(Request request) {
-        RequestBuilder builder = new RequestBuilder(request);
-        return this.executeRequest(builder);
-    }
+	public TracingAsyncHttpClient(Tracer tracer, SpanDecorator spanDecorator, AsyncHttpClientConfig config) {
+		super(config);
+		this.tracer = tracer;
+		this.spanDecorator = spanDecorator;
+		// Retain a reference to futures so they aren't GC'ed before completion.
+		this.traceFutures = new ArrayList<>();
+	}
 
-    @Override
-    public ListenableFuture<Response> executeRequest(RequestBuilder builder) {
-        return tracedFuture(builder,
-                (requestBuilder) -> super.executeRequest(requestBuilder.build()));
-    }
+	@Override
+	public ListenableFuture<Response> executeRequest(Request request) {
+		return executeRequest(request, new AsyncCompletionHandlerBase());
+	}
 
-    @Override
-    public <T> ListenableFuture<T> executeRequest(Request request, AsyncHandler<T> handler) {
-        RequestBuilder builder = new RequestBuilder(request);
-        return this.executeRequest(builder, handler);
-    }
+	@Override
+	public ListenableFuture<Response> executeRequest(RequestBuilder builder) {
+		return executeRequest(builder.build());
+	}
 
-    @Override
-    public <T> ListenableFuture<T> executeRequest(RequestBuilder builder, AsyncHandler<T> handler) {
-        return tracedFuture(builder,
-                (requestBuilder) -> super.executeRequest(requestBuilder.build(), handler));
-    }
+	@Override
+	public <T> ListenableFuture<T> executeRequest(Request request, AsyncHandler<T> handler) {
+		return internalExecuteRequest(request, handler);
+	}
 
-    private <T> ListenableFuture<T> tracedFuture(RequestBuilder requestBuilder, ListenableFutureGenerator<T> generator) {
-        Tracer.SpanBuilder builder = tracer.buildSpan(spanDecorator.getOperationName(requestBuilder));
+	@Override
+	public <T> ListenableFuture<T> executeRequest(RequestBuilder builder, AsyncHandler<T> handler) {
+		return executeRequest(builder.build(), handler);
+	}
 
-        Span parent = activeSpanSource.getActiveSpan();
-        if (parent != null) {
-            System.out.println("ACTIVE_SPAN_LIVE"); // TODO: REMOVE
-            builder.asChildOf(parent);
-        } else { // TODO: REMOVE
-            System.out.println("ACTIVE_SPAN_NULL");
-        }
+	private <T> ListenableFuture<T> internalExecuteRequest(Request request, AsyncHandler<T> handler) {
 
-        spanDecorator.decorateSpan(builder, requestBuilder);
+		Tracer.SpanBuilder builder = tracer.buildSpan(spanDecorator.getOperationName(request));
 
-        final Span span = builder.start();
-        tracer.inject(span.context(), Format.Builtin.HTTP_HEADERS, new TextMap() {
-            @Override
-            public Iterator<Map.Entry<String, String>> iterator() {
-                throw new UnsupportedOperationException("iterator should never be used with Tracer.inject()");
-            }
+		Span parent = tracer.activeSpan();
+		if (parent != null) {
+			System.out.println("ACTIVE_SPAN_LIVE"); // TODO: REMOVE
+			builder.asChildOf(parent);
+		} else { // TODO: REMOVE
+			System.out.println("ACTIVE_SPAN_NULL");
+		}
 
-            @Override
-            public void put(String key, String value) {
-                requestBuilder.addHeader(key, value);
-            }
-        });
-        ListenableFuture<T> lf = generator.generateFuture(requestBuilder);
-        traceFutures.add(lf.toCompletableFuture().whenComplete(
-                (t, throwable) -> {
-                    span.finish();
-                    clearFinished(traceFutures);
-                }));
-        return lf;
-    }
+		spanDecorator.decorateSpan(builder, request);
 
-    private static void clearFinished(List<CompletableFuture<?>> futures) {
-        Iterator<CompletableFuture<?>> iter = futures.iterator();
-        while (iter.hasNext()) {
-            if (iter.next().isDone()) {
-                iter.remove();
-            }
-        }
-    }
+		final Span span = builder.start();
+		RequestBuilder requestBuilder = new RequestBuilder(request);
 
-    /**
-     * An Abstract API that allows the TracingAsyncHttpClient to customize how spans are named and decorated.
-     */
-    @SuppressWarnings("WeakerAccess")
-    public interface SpanDecorator {
-        /**
-         * @param requestBuilder the request that a span is being constructed for.
-         * @return the operation name a span should use for this request.
-         */
-        String getOperationName(RequestBuilder requestBuilder);
+		tracer.inject(span.context(), Format.Builtin.HTTP_HEADERS, new TextMap() {
+			@Override
+			public Iterator<Map.Entry<String, String>> iterator() {
+				throw new UnsupportedOperationException("iterator should never be used with Tracer.inject()");
+			}
 
-        /**
-         * Adds data to a span based on the contents of the request.
-         * @param span the span for an operation.
-         * @param requestBuilder the request that represents the operation.
-         */
-        void decorateSpan(Tracer.SpanBuilder span, RequestBuilder requestBuilder);
+			@Override
+			public void put(String key, String value) {
+				requestBuilder.addHeader(key, value);
+			}
+		});
+		// by activate span, RequestFilter can be aware of active span.
+		// in ResponseFilter and IOExceptionFilter, we can cast handler to Traceable
+		try (Scope scope = tracer.scopeManager().activate(span, false)) {
+			ListenableFuture<T> lf = super.executeRequest(requestBuilder.build(),
+					new SpanAwareAsyncHandler<T>(handler, span, tracer));
+			traceFutures.add(lf.toCompletableFuture().whenComplete((t, throwable) -> {
+				span.finish();
+				clearFinished(traceFutures);
+			}));
 
+			return lf;
+		}
+	}
 
-        @SuppressWarnings("unused")
-        SpanDecorator DEFAULT = new SpanDecorator() {
-            @Override
-            public String getOperationName(RequestBuilder requestBuilder) {
-                Request request = requestBuilder.build();
-                return request.getUri().getHost() + ":" + request.getMethod();
-            }
+	private static void clearFinished(List<CompletableFuture<?>> futures) {
+		Iterator<CompletableFuture<?>> iter = futures.iterator();
+		while (iter.hasNext()) {
+			if (iter.next().isDone()) {
+				iter.remove();
+			}
+		}
+	}
 
-            @Override
-            public void decorateSpan(Tracer.SpanBuilder span, RequestBuilder request) {
-                // Be default, do nothing.
-            }
-        };
-    }
+	/**
+	 * An Abstract API that allows the TracingAsyncHttpClient to customize how spans
+	 * are named and decorated.
+	 */
 
-    /**
-     * An abstract API that allows the TracingAsyncHttpClient to customize how parent Spans are discovered.
-     *
-     * For instance, if Spans are stored in a thread-local variable, an ActiveSpanSource could access them like so:
-     * <p>Example usage:
-     * <pre>{@code
-     * public class SomeClass {
-     *     // Thread local variable containing each thread's ID
-     *     private static final ThreadLocal<Span> activeSpan =
-     *         new ThreadLocal<Span>() {};
-     *
-     * ... elsewhere ...
-     * ActiveSpanSource spanSource = new ActiveSpanSource() {
-     *     public Span activeSpan() {
-     *         return activeSpan.get();
-     *     }
-     * };
-     *
-     * ...
-     * }
-     *}</pre>
-     */
-    @SuppressWarnings("WeakerAccess")
-    public interface ActiveSpanSource {
-        /**
-         * Get the currently active Span to serve as a parent span for new spans.
-         *
-         * @return the currently active span, or null.
-         */
-        Span getActiveSpan();
-    }
+	public interface SpanDecorator {
+		/**
+		 * @param request
+		 *            the request that a span is being constructed for.
+		 * @return the operation name a span should use for this request.
+		 */
+		String getOperationName(Request request);
 
-    private interface ListenableFutureGenerator<T> {
-        ListenableFuture<T> generateFuture(RequestBuilder requestBuilder);
-    }
+		/**
+		 * Adds data to a span based on the contents of the request.
+		 * 
+		 * @param span
+		 *            the span for an operation.
+		 * @param request
+		 *            the request that represents the operation.
+		 */
+		void decorateSpan(Tracer.SpanBuilder span, Request request);
+
+		@SuppressWarnings("unused")
+		SpanDecorator DEFAULT = new SpanDecorator() {
+			@Override
+			public String getOperationName(Request request) {
+				return request.getUri().getHost() + ":" + request.getMethod();
+			}
+
+			@Override
+			public void decorateSpan(Tracer.SpanBuilder span, Request request) {
+				// Be default, do nothing.
+			}
+		};
+	}
+
+	public interface Traceable {
+
+		Span getSpan();
+
+		Tracer getTracer();
+
+	}
+
+	private static class SpanAwareAsyncHandler<T> implements AsyncHandler<T>, Traceable {
+
+		private final AsyncHandler<T> delegate;
+		private final Span span;
+		private final Tracer tracer;
+
+		SpanAwareAsyncHandler(AsyncHandler<T> delegate, Span span, Tracer tracer) {
+			this.delegate = delegate;
+			this.span = span;
+			this.tracer = tracer;
+		}
+
+		@Override
+		public Span getSpan() {
+			return span;
+		}
+
+		@Override
+		public Tracer getTracer() {
+			return tracer;
+		}
+
+		@Override
+		public State onStatusReceived(HttpResponseStatus responseStatus) throws Exception {
+			try (Scope socpe = tracer.scopeManager().activate(span, false)) {
+				return delegate.onStatusReceived(responseStatus);
+			}
+		}
+
+		@Override
+		public State onHeadersReceived(HttpHeaders headers) throws Exception {
+			try (Scope socpe = tracer.scopeManager().activate(span, false)) {
+				return delegate.onHeadersReceived(headers);
+			}
+		}
+
+		@Override
+		public State onBodyPartReceived(HttpResponseBodyPart bodyPart) throws Exception {
+			try (Scope socpe = tracer.scopeManager().activate(span, false)) {
+				return delegate.onBodyPartReceived(bodyPart);
+			}
+		}
+
+		@Override
+		public void onThrowable(Throwable t) {
+			try (Scope socpe = tracer.scopeManager().activate(span, false)) {
+				delegate.onThrowable(t);
+			}
+		}
+
+		@Override
+		public T onCompleted() throws Exception {
+			try (Scope socpe = tracer.scopeManager().activate(span, false)) {
+				return delegate.onCompleted();
+			}
+		}
+
+	}
+
 }
